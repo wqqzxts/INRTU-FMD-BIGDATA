d02c54c6989fa19f5849efeb499b6f550899f947
Timur Semennikov <wqqzxts@outlook.ocm>
2025-04-02 16:21:36 +0800

    changed database engine to microsoft sql

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+    
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE':'mssql',                    
+        'NAME':'ResidentManagement',                       
+        'HOST':'qzxts', 
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
     }
 }
 
